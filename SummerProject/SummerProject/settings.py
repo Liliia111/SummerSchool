d3cef2d471969df30157bfc86b6620ac52bfe86b
AndriyStasyuk <andrii.stasiuk.o@gmail.com>
2019-07-26 10:36:17 +0300

    Add templates and new app

--- SummerProject/SummerProject/settings.py
+++ SummerProject/SummerProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -51,10 +52,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'SummerProject.urls'
 
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
