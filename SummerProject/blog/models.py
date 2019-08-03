478de399e3ea0f3bac7e486e28ca0bf08124b14f
mishafil <mishafilipenko2@gmail.com>
2019-08-04 00:56:54 +0300

    Add post method

--- SummerProject/blog/models.py
+++ SummerProject/blog/models.py
@@ -8,4 +8,4 @@ class Article(models.Model):
     content = models.CharField(max_length=1000)
 
     def __str__(self):
-        return str(self.title) + ", " + str(self.content)
+        return self.title
