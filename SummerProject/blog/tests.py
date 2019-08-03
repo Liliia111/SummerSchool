2c7b6ab3b93f5892b5b4e3f22a21b691dfa2eeae
mishafil <mishafilipenko2@gmail.com>
2019-08-04 00:55:14 +0300

    Add post method

--- SummerProject/blog/tests.py
+++ SummerProject/blog/tests.py
@@ -1,16 +1,2 @@
-import requests
 
-
-def test_fail():
-    response = requests.get('http://127.0.0.1:8000/')
-    assert response.status_code == 404
-    assert not response.is_redirect
-
-
-def test_all_articles():
-    response = requests.get('http://127.0.0.1:8000/blog/api/v1/articles/')
-    response.raise_for_status()
-    assert response.status_code == 200
-    assert not response.is_redirect
-    assert response.headers["Content-Type"] == "application/json"
 
