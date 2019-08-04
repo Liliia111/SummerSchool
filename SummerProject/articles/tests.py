92d11506f55facb2930215bcd585d6d1715b3968
MaxymMartynyak <maxmartynyak@gmail.com>
2019-08-05 00:56:33 +0300

    some tests

--- SummerProject/articles/tests.py
+++ SummerProject/articles/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import unittest
+import requests
 
-# Create your tests here.
+
+class Test(unittest.TestCase):
+    def test_get(self):
+        tmp = requests.get("http://127.0.0.1:8000/api/v1/articles/")
+        self.assertEqual(tmp.status_code, 200)
+        self.assertFalse(tmp.is_redirect)
+        self.assertEqual(tmp.headers['Content-Type'], "application/json")
+
+    def test_post(self):
+        tmp = requests.post("http://127.0.0.1:8000/api/v1/articles/", json={"title": "new_title", "content": "new_content"})
+        self.assertEqual(tmp.status_code, 201)
+        self.assertFalse(tmp.is_redirect)
+        self.assertEqual(tmp.headers['Content-Type'], "application/json")
+        # self.assertEqual(tmp.text, {"title": "new_title", "content": "new_content"})
