eed5e9ad0f0918939b6fd164e3a3074ad6c6455d
MaxymMartynyak <maxmartynyak@gmail.com>
2019-08-10 15:08:30 +0300

    new tests

--- SummerProject/articles/tests/test_article_details.py
+++ SummerProject/articles/tests/test_article_details.py
@@ -1,13 +1,34 @@
 import pytest
+from articles.models import Article
 
 
-def test_get_by_pk(client):
-    # data = {
-    #     'title': 'new title',
-    #     'content': 'new content'
-    # }
-    # posting = client.post('api/v1/articles/', data=data, type='')
-    response = client.get('api/v1/articles/')
+@pytest.fixture()
+def new_article():
+    return Article.objects.create(title="new title", content="new content")
+
+
+@pytest.mark.django_db
+def test_get(client, new_article):
+    response = client.get('/api/v1/articles/{}/'.format(new_article.id))
     assert response.status_code == 200
-    # assert response.json() == {"title":	"new title", "content": "new content"}
+    assert response.json() == {"title":	new_article.title, "content": new_article.content}
+    assert response['custom_id'] == str(new_article.id)
+    assert response['Content-Type'] == 'application/json'
 
+
+@pytest.mark.django_db
+def test_put(client, new_article):
+    data = {
+        "title": "new title",
+        "content": "new content"
+    }
+    response = client.put('/api/v1/articles/{}/'.format(new_article.id), data=data, content_type='application/json')
+    assert response.status_code == 200
+    assert response['Content-Type'] == 'application/json'
+
+# test delete method
+@pytest.mark.django_db
+def test_delete(client, new_article):
+    response = client.delete('/api/v1/articles/{}/'.format(new_article.id))
+
+    assert response.status_code == 204
--- SummerProject/articles/tests/test_articles.py
+++ SummerProject/articles/tests/test_articles.py
@@ -1,4 +1,10 @@
 import pytest
+from articles.models import Article
+
+
+@pytest.fixture()
+def new_article():
+    return Article.objects.create(title="new title", content="new content")
 
 
 @pytest.mark.django_db
@@ -8,17 +14,28 @@ def test_fail(client):
 
 
 @pytest.mark.django_db
-def test_get_all_articles(client):
+def test_get(client):
     response = client.get('/api/v1/articles/')
     assert response.status_code == 200
+    assert response['Content-Type'] == "text/html; charset=utf-8"
+    assert response['custom'] == "custom get articles"
 
 
-@pytest.mark.django_db
-def test_post_article(client):
+@pytest.mark.parametrize("title_text, content_text", [
+    ("title" * 10, "content" * 50),
+    ("title" * 25, "content" * 100),
+    ("title" * 70, "content" * 300),
+    ("title" * 125, "content" * 700)
+])
+@ pytest.mark.django_db
+def test_post(client, title_text, content_text, ):
     data = {
-        "title": "fixture title",
-        "content": "fixture content"
+        "title": title_text,
+        "content": content_text
     }
     response = client.post('/api/v1/articles/', data=data, content_type='application/json')
-    assert response.status_code == 200
-    assert response.json() == {'created': {"title": "fixture title", "content": "fixture content"}}
+    if len(data['title']) > 300:
+        assert response.status_code == 400
+    else:
+        assert response.status_code == 201
+        assert response["Content-Type"] == "application/json"
