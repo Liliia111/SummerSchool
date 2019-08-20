61b8794e5326481857b802580b88f047aecbb8db
AlexK0210 <knjupaa@gmail.com>
2019-08-20 14:57:02 +0300

    updated models

--- SummerProject/user_auth/models.py
+++ SummerProject/user_auth/models.py
@@ -2,7 +2,12 @@ from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import UserManager
 
+from enum import Enum
 
+class SocialNetworkChoice(Enum):
+    FB = "Facebook"
+    TR = "Twitter"
+    GGL = "Google"
 class Role(models.Model):
     role = models.CharField(max_length=254)
 
@@ -37,3 +42,32 @@ class User(AbstractBaseUser):
             self.last_name = last_name
 
         self.save()
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=155, blank=False)
+    parent_category_id = models.IntegerField(blank=False)
+
+class Team(models.Model):
+    name = models.CharField(max_length=150)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Article(models.Model):
+    headline = models.CharField(max_length=150, blank=False)
+    photo = models.ImageField(max_length=150)
+    video = models.URLField(max_length=150)
+    author = models.CharField(max_length=55, blank=False)
+    source = models.CharField(max_length=3, choices=[(tag, tag.value) for tag in SocialNetworkChoice])
+    content = models.TextField(blank=False)
+    team = models.ForeignKey(Team)
+    category = models.ForeignKey(Category)
+
+class Comment(models.Model):
+    content = models.CharField(max_length=1000)
+    date = models.DateTimeField()
+    user = models.ForeignKey(User)
+
+class ArticleComment(Article, Comment):
+    article = models.ForeignKey(Article)
+    comment = models.ForeignKey(Comment)
