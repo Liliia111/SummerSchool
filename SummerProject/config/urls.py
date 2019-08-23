10e0d70bf8b42e536131a7ecefa3959c6b5f12b3
AndriyStasyuk <andrii.stasiuk.o@gmail.com>
2019-08-23 16:43:41 +0300

    Update UI settings

--- SummerProject/config/urls.py
+++ SummerProject/config/urls.py
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from main import views
 
 urlpatterns = [
-    path('', views.index),
     path('admin/', admin.site.urls),
-    path('api/v1/user/', include('user.urls'))
+    path('api/v1/user/', include('user.urls')),
+    re_path(r'.*', views.index),
 ]
