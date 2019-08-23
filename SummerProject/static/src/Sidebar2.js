50d65c82f582d9991c90dff5601d240d2c61bf7b
Liliia111 <liliia.ryvak@gmail.com>
2019-08-23 16:50:53 +0300

    change sidebar

--- SummerProject/static/src/Sidebar2.js
+++ SummerProject/static/src/Sidebar2.js
@@ -1,23 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { makeStyles, withStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import axios from 'axios';
-import { createMuiTheme } from '@material-ui/core/styles';
-import { sizing } from '@material-ui/system';
-const drawerWidth = 253;
 import './style3.css'
+import axios from 'axios';
+
+const drawerWidth = 253;
+
 
 class PermanentDrawerLeft2 extends React.Component {
  state = {
@@ -38,7 +25,7 @@ return (
 <ul className="list" aria-haspopup="true">
  {this.state.categories.map((text) => (
      <li className="active">
-         <a href="#homeSubmenu" className="first-level" >{text}</a>
+         <a href="#" className="first-level" >{text}</a>
       </li>
 
 
