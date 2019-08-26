3dc6a581781dd8e150df7f7019a7778e2f74bed8
Liliia111 <liliia.ryvak@gmail.com>
2019-08-26 11:41:32 +0300

    update app

--- SummerProject/static/src/app.js
+++ SummerProject/static/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
-
+import Sidebar2 from './components/sidebar/Sidebar2'
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import MainRoutes from './router';
 
@@ -9,6 +9,7 @@ const App = () => (
     <Router>
         <MuiThemeProvider>
             <MainRoutes/>
+            <Sidebar2 />
         </MuiThemeProvider>
     </Router>
 );
