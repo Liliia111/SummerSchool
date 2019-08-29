a23bd1c27cffaa3800d78a01d9be79f8e269d768
Liliia111 <liliia.ryvak@gmail.com>
2019-08-30 00:02:28 +0300

    add sidebar to home

--- SummerProject/static/src/components/Home/index.js
+++ SummerProject/static/src/components/Home/index.js
@@ -1,21 +1,14 @@
 import React from "react";
-<<<<<<< HEAD
-import Sidebar from "../Sidebar";
-
-class Home extends React.Component {
-  render() {
-    return <Sidebar />;
-  }
-=======
 import Footer from '../Footer'
+import Sidebar from '../Sidebar'
 
 class Home extends React.Component {
     render() {
         return <>
+            <Sidebar/>
             <Footer/>
         </>
     }
->>>>>>> e073b0ee43a01cc552827134909811d44774e4ec
 }
 
 export default Home;
--- SummerProject/static/src/router.js
+++ SummerProject/static/src/router.js
@@ -1,11 +1,10 @@
+/* eslint max-len: ["error", { "ignoreStrings": true }]*/
+
 import React from 'react';
 import {Route, Switch, Redirect} from 'react-router-dom';
 import Registration from './components/Registration'
 import Home from "./components/Home";
-<<<<<<< HEAD
-=======
 
-const Home =()=><div>Home</div>
 
 export default class MainRoutes extends React.Component {
   /**
