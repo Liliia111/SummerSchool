73c2d7992e888500078364e9704d1fae7dc0f2cc
Liliia111 <liliia.ryvak@gmail.com>
2019-09-01 15:00:59 +0300

    add urls for password reset

--- SummerProject/user/urls.py
+++ SummerProject/user/urls.py
@@ -1,12 +1,14 @@
 # pylint: disable=C0103
 """Url for user app"""
-
+from django.conf.urls import url
 from django.urls import path
-from .views import registration, login, logout, UserView
+from .views import registration, login, logout, UserView, forgot_password_email_send, forgot_password_reset_confirm
 
 urlpatterns = [
     path('registration/', registration),
     path('login/', login),
     path('logout/', logout),
-    path('self/', UserView.as_view())
+    path('self/', UserView.as_view()),
+    path('forgotPassword/', forgot_password_email_send),
+    url(r'^reset_password_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', forgot_password_reset_confirm)
 ]
