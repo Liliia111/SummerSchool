a5428e8c8a9fe1dc04633987c5384d8fa6c8daea
Liliia111 <liliia.ryvak@gmail.com>
2019-09-03 00:40:39 +0300

    add url

--- SummerProject/user/urls.py
+++ SummerProject/user/urls.py
@@ -2,7 +2,8 @@
 """Url for user app"""
 from django.conf.urls import url
 from django.urls import path
-from .views import registration, login, logout, UserView, forgot_password_email_send, forgot_password_reset_confirm
+from .views import registration, login, logout, UserView, forgot_password_email_send, forgot_password_reset_confirm, \
+    forgot_password_handler
 
 urlpatterns = [
     path('registration/', registration),
@@ -10,5 +11,6 @@ urlpatterns = [
     path('logout/', logout),
     path('self/', UserView.as_view()),
     path('forgot_password/', forgot_password_email_send),
-    url(r'^password_reset_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', forgot_password_reset_confirm, name='password_reset_confirm')
+    url(r'^password_reset_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', forgot_password_reset_confirm, name='password_reset_confirm'),
+    path('forgot_password_handler/', forgot_password_handler)
 ]
