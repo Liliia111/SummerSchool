8e8b23c6930339f5a999d869ac7fdf7f05014e9b
Liliia111 <liliia.ryvak@gmail.com>
2019-09-02 21:48:15 +0300

    fix url

--- SummerProject/user/urls.py
+++ SummerProject/user/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('logout/', logout),
     path('self/', UserView.as_view()),
     path('forgot_password/', forgot_password_email_send),
-    url(r'^reset_password_confirm/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$', forgot_password_reset_confirm)
+    url(r'^password_reset_confirm/(?P<uidb64>[0-9A-Za-z]+)/(?P<token>.+)/$', forgot_password_reset_confirm, name='password_reset_confirm')
 ]
