fed390016922f8d042265d8097d4daa8b9cf3396
AlexK0210 <knjupaa@gmail.com>
2019-09-13 21:30:30 +0300

    final_update

--- SummerProject/articles/urls.py
+++ SummerProject/articles/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib import admin
 from articles import views
+from
 
 
 urlpatterns = [
     path('<int:pk>/', views.ArticleDetailView.as_view()),
     path('admin/', admin.site.urls),
+    path('<int:article_id>/comments/', comments_view),
+    path('most_commented/', most_commented),
 ]
