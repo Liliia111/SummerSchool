9bcb3a79fb40097b0e7fa499249714e5d2348e26
AlexK0210 <knjupaa@gmail.com>
2019-09-13 21:02:23 +0300

    fixed material-ui component and route to an article

--- SummerProject/static/src/router.js
+++ SummerProject/static/src/router.js
@@ -5,11 +5,11 @@ import {Route, Switch, Redirect} from 'react-router-dom';
 import Registration from './components/Registration'
 import LogIn from './components/LogIn'
 import Home from "./components/Home";
+import Articles from "./components/Articles";
 import CheckEmail from "./components/CheckEmail"
 import ForgotPassword from "./components/ForgotPassword";
 import ResetPassword from "./components/ResetPassword";
 
-import ArticlesView from "./components/ArticlesView";
 export default class MainRoutes extends React.Component {
   /**
     @return {} - Switch components
@@ -21,10 +21,10 @@ export default class MainRoutes extends React.Component {
           <Route path='/home/' component={Home} />
           <Route path='/registration' component={Registration} />
           <Route path='/login' component={LogIn} />
+          <Route path='/articles/:id' component={Articles} />
           <Route path='/check-email' component={CheckEmail} />
           <Route path='/forgotPassword' component={ForgotPassword} />
           <Route path='/resetPassword' component={ResetPassword}/>
-          <Route path='/articles' component={ArticlesView} />
           <Redirect path='*' to='/home/' />
         </Switch>
       </main>
