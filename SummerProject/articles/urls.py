b58b89b3511a3d676defb85e065e19684d8b3a35
Liliia111 <liliia.ryvak@gmail.com>
2019-09-19 19:29:37 +0300

    remove useless url

--- SummerProject/articles/urls.py
+++ SummerProject/articles/urls.py
@@ -6,7 +6,6 @@ from .views import ArticleDetailView, comments_view, most_commented, MostPopular
 
 urlpatterns = [
     path('<int:pk>/', ArticleDetailView.as_view()),
-    path('admin/', admin.site.urls),
     path('<int:article_id>/comments/', comments_view),
     path('most_commented/', most_commented),
     url(r'^(?P<pk>\d+)/$', ArticleCountHitDetailView.as_view(), name='article_count'),
