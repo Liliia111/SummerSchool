3d834bde870b8c3aa892b6e5b16a4f90aecda204
MaxymMartynyak <maxmartynyak@gmail.com>
2019-09-24 01:44:35 +0300

    cms app

--- SummerProject/articles/urls.py
+++ SummerProject/articles/urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls import url
 from django.urls import path
-from django.contrib import admin
 from .views import ArticleDetailView, comments_view, most_commented, MostPopularView, \
     ArticleCountHitDetailView
 
