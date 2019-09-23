3f2ac0934a67a9e766f692476d2d74a79e7fa45c
MaxymMartynyak <maxmartynyak@gmail.com>
2019-09-24 02:52:30 +0300

    create article

--- SummerProject/cms/urls.py
+++ SummerProject/cms/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import url
+from django.urls import path
+from .views import CmsArticleView
 
 urlpatterns = [
-
+    path('article/', CmsArticleView.as_view()),
 ]
--- SummerProject/cms/views.py
+++ SummerProject/cms/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseBadRequest
+from django.views import View
+import json
+from articles.models import Article
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+
 
 # Create your views here.
+class CmsArticleView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(CmsArticleView, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request):
+        # here must be validation method
+        data = request.body.decode('utf8')
+        data = json.loads(data)
+        article = Article(headline=data["headline"], photo=data["photo"], video=data["video"], author=request.user,
+                          source=data["source"], content=data["content"], team=data["team"],
+                          category=data["category"])
+        article.save()
+        response = HttpResponse(status=201)
+        response['article_id'] = article.id
+        return response
